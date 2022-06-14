59d94d0d7e029fa26ba146ebc85239e8036da5c5
cguarinm <95106066+cguarinm@users.noreply.github.com>
2022-06-13 20:46:24 -0500

    Add files via upload

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys, operator
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    data = []
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        lineas = line.replace("\n", "")
+        lineas = line.split('\t')
+        data.append(lineas)
+
+    dataord = sorted(data, key=operator.itemgetter(1), reverse=False)
+
+    for row in dataord:
+        sys.stdout.write("{},{}".format(row[0], row[1]))
