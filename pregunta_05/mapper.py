6fa96944463740052b73d9e8e2e64e1d7bc71d7b
cguarinm <95106066+cguarinm@users.noreply.github.com>
2022-06-15 00:14:17 -0500

    Add files via upload

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        #se separan las columnas separadas
+        lista = line.split('   ')
+        mes = lista[1].split('-')
+        #se genera la salida del dato llamando la columna objetivo
+        sys.stdout.write("{}\t1\n".format(mes[1]))
