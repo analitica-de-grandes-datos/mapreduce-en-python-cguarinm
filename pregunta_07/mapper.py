9b81002fc48922ddd006fa30f79df89d55aff969
cguarinm <95106066+cguarinm@users.noreply.github.com>
2022-06-16 23:41:01 -0500

    Add files via upload

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        
+        lista = line.split('   ')
+        val = line.split('   ')[2].strip()
+        val = val.zfill(4)
+        
+        sys.stdout.write("{}\t{}\t{}\n".format(lista[0], val, lista[1]))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        lista = line.split('\t')
+        val = line.split('\t')[1]
+        val = int(val)
+
+        sys.stdout.write("{}   {}   {}\n".format(
+            lista[0], lista[2].strip(), val))
