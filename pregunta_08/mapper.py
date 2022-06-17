e5e7596b9e6464b77ec72e46fa500b52e17a0ca7
cguarinm <95106066+cguarinm@users.noreply.github.com>
2022-06-16 23:54:20 -0500

    Add files via upload

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        
+        lista = line.split('   ')
+        
+        sys.stdout.write("{}\t{}\n".format(lista[0], float(lista[2])))
